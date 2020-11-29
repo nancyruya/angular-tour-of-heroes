80ac791ee0efadf3ff517327dfa485791b1f2344
nancyruya <nancyruya@gmail.com>
2020-11-29 13:48:26 -0500

    added lists of heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,14 +8,26 @@ import { Hero } from "../interfaces/Hero";
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
+  heroes: Hero[] = [
+    { id: 11, name: 'Dr Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+  ];
 
-  constructor() { }
+  selectedHero: Hero;  //property go before constructor 
+
+
 
   ngOnInit(): void {
   }
+
+
 
 }
