ca7485de540d528d19610920537bd07b763b96b0
nancyruya <nancyruya@gmail.com>
2020-12-01 20:23:00 -0500

    add additional messagesto hero service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -4,28 +4,32 @@ import { Hero } from "../interfaces/Hero";
 
 import { HeroService } from '../services/hero.service';
 
+import { MessageService } from '../services/message.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+
+  selectedHero: Hero;
+
   heroes: Hero[];
 
-  selectedHero: Hero;  //property go before constructor 
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
-  constructor(private heroService: HeroService) { }
-
-  ngOnInit(): void {
+  ngOnInit() {
     this.getHeroes();
   }
 
-  //method go after
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 }
